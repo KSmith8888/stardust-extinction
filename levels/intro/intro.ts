1285339b9dd5de0c47a23b1ab7ec96ebdbcb3bff
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-09-15 18:03:18 -0700

    Updated intro dialog and styling

--- levels/intro/intro.ts
+++ levels/intro/intro.ts
@@ -1,4 +1,5 @@
 class Intro {
+    mainContainer: HTMLElement;
     dialogBox: HTMLDivElement;
     firstParagraph: HTMLParagraphElement;
     secondParagraph: HTMLParagraphElement;
@@ -6,6 +7,9 @@ class Intro {
     skipIntroButton: HTMLButtonElement;
     skipIntro: void;
     constructor() {
+        this.mainContainer = <HTMLElement>(
+            document.getElementById("main-container")
+        );
         this.dialogBox = <HTMLDivElement>document.getElementById("dialog-box");
         this.firstParagraph = <HTMLParagraphElement>(
             document.getElementById("dialog-first-line")
@@ -22,25 +26,41 @@ class Intro {
         });
     }
     nextPhase() {
-        setTimeout(() => {
-            if (this.currentPhase === 1) {
+        if (this.currentPhase === 1) {
+            setTimeout(() => {
                 this.currentPhase = 2;
                 this.generateText(
                     this.secondParagraph,
                     "One of these colonies, XR4-793, lies at the edge of the Milky Way Galaxy."
                 );
-            } else if (this.currentPhase === 2) {
-                const container = document.getElementById("container");
-                if (container) {
-                    container.classList.add("fadeOutElement");
-                    this.dialogBox.classList.add("fadeOutElement");
-                }
-            }
-        }, 500);
+            }, 750);
+        } else if (this.currentPhase === 2) {
+            setTimeout(() => {
+                this.currentPhase = 3;
+                this.firstParagraph.textContent = "";
+                this.secondParagraph.textContent = "";
+                intro.generateText(
+                    intro.firstParagraph,
+                    "While speculation continued that alien life may be out there, none had yet been encountered."
+                );
+            }, 1000);
+        } else if (this.currentPhase === 3) {
+            setTimeout(() => {
+                this.currentPhase = 4;
+                this.generateText(
+                    this.secondParagraph,
+                    "So, when the long-range communication system for XR4-793 went down, it was assumed to just be a technical issue. Neither the colonists or the command back on Earth could have imagined what was starting..."
+                );
+            }, 750);
+        } else if (this.currentPhase === 4) {
+            setTimeout(() => {
+                this.mainContainer.classList.add("fade-out-element");
+            }, 500);
+        }
     }
     generateText(paragraphEl: HTMLParagraphElement, text: string) {
         const textArray = text.split("");
-        const delay = 200;
+        const delay = 150;
         textArray.forEach((char, index) => {
             setTimeout(() => {
                 paragraphEl.textContent += char;
