9b5ff8454bcaa483952772815c8b4ae4daa49b18
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-10-05 17:29:57 -0700

    Added overcharge touch control

--- src/events/mouse-events.ts
+++ src/events/mouse-events.ts
@@ -3,6 +3,9 @@ import Player from "../player/player";
 export default class MouseEvents {
     player: Player;
     canvas: HTMLCanvasElement;
+    halfWidth: number;
+    halfHeight: number;
+    healthbarArea: number;
     contextMenu: void;
     mousedown: void;
     mouseup: void;
@@ -10,6 +13,9 @@ export default class MouseEvents {
     constructor(player: Player, canvas: HTMLCanvasElement) {
         this.player = player;
         this.canvas = canvas;
+        this.halfWidth = this.player.width / 2;
+        this.halfHeight = this.player.height / 2;
+        this.healthbarArea = canvas.height - 35;
         this.contextMenu = this.canvas.addEventListener("contextmenu", (e) => {
             e.preventDefault();
         });
@@ -20,7 +26,7 @@ export default class MouseEvents {
                     e.offsetX <= this.player.x + this.player.width &&
                     e.offsetY >= this.player.y &&
                     e.offsetY <= this.player.y + this.player.height &&
-                    e.offsetY < this.canvas.height - 35 &&
+                    e.offsetY < this.healthbarArea &&
                     !this.player.isShipDisabled
                 ) {
                     this.player.isMoving = true;
@@ -36,22 +42,22 @@ export default class MouseEvents {
         });
         this.mousemove = canvas.addEventListener("mousemove", (e): void => {
             if (this.player.isMoving && !this.player.isShipDisabled) {
-                if (e.offsetX + this.player.width / 2 > canvas.width) {
+                if (e.offsetX + this.halfWidth > canvas.width) {
                     this.player.x = canvas.width - this.player.width;
-                } else if (e.offsetX - this.player.width / 2 < 0) {
+                } else if (e.offsetX - this.halfWidth < 0) {
                     this.player.x = 0;
                 } else {
-                    this.player.x = e.offsetX - this.player.width / 2;
+                    this.player.x = e.offsetX - this.halfWidth;
                 }
                 if (
-                    e.offsetY - this.player.height / 2 >
+                    e.offsetY - this.halfHeight >
                     canvas.height - (this.player.height + 30)
                 ) {
                     this.player.y = canvas.height - (this.player.height + 30);
-                } else if (e.offsetY - this.player.height / 2 < 40) {
+                } else if (e.offsetY - this.halfHeight < 40) {
                     this.player.y = 40;
                 } else {
-                    this.player.y = e.offsetY - this.player.height / 2;
+                    this.player.y = e.offsetY - this.halfHeight;
                 }
             }
         });
--- src/events/touch-events.ts
+++ src/events/touch-events.ts
@@ -3,19 +3,27 @@ import Player from "../player/player";
 export default class TouchEvents {
     player: Player;
     canvas: HTMLCanvasElement;
+    halfWidth: number;
+    halfHeight: number;
+    healthbarArea: number;
     touchstart: void;
     touchend: void;
     touchmove: void;
     constructor(player: Player, canvas: HTMLCanvasElement) {
         this.player = player;
         this.canvas = canvas;
+        this.halfWidth = this.player.width / 2;
+        this.halfHeight = this.player.height / 2;
+        this.healthbarArea = canvas.height - 35;
         this.touchstart = canvas.addEventListener("touchstart", (e): void => {
-            if (
+            if (e.touches[0] && e.touches[1]) {
+                this.player.useSpecialAttack();
+            } else if (
                 e.touches[0].clientX >= this.player.x &&
                 e.touches[0].clientX <= this.player.x + this.player.width &&
                 e.touches[0].clientY >= this.player.y &&
                 e.touches[0].clientY <= this.player.y + this.player.height &&
-                e.touches[0].clientY < canvas.height - 35 &&
+                e.touches[0].clientY < this.healthbarArea &&
                 !this.player.isShipDisabled
             ) {
                 this.player.isMoving = true;
@@ -28,27 +36,22 @@ export default class TouchEvents {
         });
         this.touchmove = canvas.addEventListener("touchmove", (e): void => {
             if (this.player.isMoving && !this.player.isShipDisabled) {
-                if (
-                    e.touches[0].clientX + this.player.width / 2 >
-                    canvas.width
-                ) {
+                if (e.touches[0].clientX + this.halfWidth > canvas.width) {
                     this.player.x = canvas.width - this.player.width;
-                } else if (e.touches[0].clientX - this.player.width / 2 < 0) {
+                } else if (e.touches[0].clientX - this.halfWidth < 0) {
                     this.player.x = 0;
                 } else {
-                    this.player.x =
-                        e.touches[0].clientX - this.player.width / 2;
+                    this.player.x = e.touches[0].clientX - this.halfWidth;
                 }
                 if (
-                    e.touches[0].clientY + this.player.height / 2 >
+                    e.touches[0].clientY + this.halfHeight >
                     canvas.height - 30
                 ) {
                     this.player.y = canvas.height - (this.player.height + 30);
-                } else if (e.touches[0].clientY - this.player.height / 2 < 40) {
+                } else if (e.touches[0].clientY - this.halfHeight < 40) {
                     this.player.y = 40;
                 } else {
-                    this.player.y =
-                        e.touches[0].clientY - this.player.height / 2;
+                    this.player.y = e.touches[0].clientY - this.halfHeight;
                 }
             }
         });
