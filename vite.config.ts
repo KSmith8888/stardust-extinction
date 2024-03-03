e1970ae197d4d00cddc1c0d8257a98cf9a5320f3
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-03-02 16:53:08 -0800

    Updated paths for new build

--- vite.config.ts
+++ vite.config.ts
@@ -8,28 +8,25 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                intro: resolve(
-                    `${__dirname}/stardust-extinction/levels/intro/`,
-                    "intro.html"
-                ),
+                intro: resolve(`${__dirname}/levels/intro/`, "intro.html"),
                 tutorial: resolve(
-                    `${__dirname}/stardust-extinction/levels/tutorial/`,
+                    `${__dirname}/levels/tutorial/`,
                     "tutorial.html"
                 ),
                 levelOne: resolve(
-                    `${__dirname}/stardust-extinction/levels/level-1/`,
+                    `${__dirname}/levels/level-1/`,
                     "level-1.html"
                 ),
                 levelTwo: resolve(
-                    `${__dirname}/stardust-extinction/levels/level-2/`,
+                    `${__dirname}/levels/level-2/`,
                     "level-2.html"
                 ),
                 levelThree: resolve(
-                    `${__dirname}/stardust-extinction/levels/level-3/`,
+                    `${__dirname}/levels/level-3/`,
                     "level-3.html"
                 ),
                 levelFour: resolve(
-                    `${__dirname}/stardust-extinction/levels/level-4/`,
+                    `${__dirname}/levels/level-4/`,
                     "level-4.html"
                 ),
             },
