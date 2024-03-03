8613776b2dfaa1a245fb3b197260e1a35aefbe86
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-03-02 16:39:42 -0800

    Updated config paths for new host

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,34 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/stardust-extinction/",
     build: {
         target: ["es2021", "edge88", "firefox98", "chrome87", "safari15.4"],
         assetsInlineLimit: 0,
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                intro: resolve(`${__dirname}/levels/intro/`, "intro.html"),
+                intro: resolve(
+                    `${__dirname}/stardust-extinction/levels/intro/`,
+                    "intro.html"
+                ),
                 tutorial: resolve(
-                    `${__dirname}/levels/tutorial/`,
+                    `${__dirname}/stardust-extinction/levels/tutorial/`,
                     "tutorial.html"
                 ),
                 levelOne: resolve(
-                    `${__dirname}/levels/level-1/`,
+                    `${__dirname}/stardust-extinction/levels/level-1/`,
                     "level-1.html"
                 ),
                 levelTwo: resolve(
-                    `${__dirname}/levels/level-2/`,
+                    `${__dirname}/stardust-extinction/levels/level-2/`,
                     "level-2.html"
                 ),
                 levelThree: resolve(
-                    `${__dirname}/levels/level-3/`,
+                    `${__dirname}/stardust-extinction/levels/level-3/`,
                     "level-3.html"
                 ),
                 levelFour: resolve(
-                    `${__dirname}/levels/level-4/`,
+                    `${__dirname}/stardust-extinction/levels/level-4/`,
                     "level-4.html"
                 ),
             },
